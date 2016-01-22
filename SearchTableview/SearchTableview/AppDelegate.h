1b1bfeb28bce9267aab5407b250f790ad9188308
liyang_a <771510512@qq.com>
2016-01-22 09:28:04 +0800

    项目搭建

---
+++ SearchTableview/SearchTableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchTableview
+//
+//  Created by 李阳 on 16/1/22.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchTableview/SearchTableview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SearchTableview
+//
+//  Created by 李阳 on 16/1/22.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
